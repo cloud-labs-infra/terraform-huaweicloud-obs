3c10105b2e6d31c9b23dd1d67c2c8214c22b1daa
Titov Aleksandr <t.sander.v@gmail.com>
2023-04-10 18:13:00 +0300

    feat: Add module Huawei Cloud OBS

---
+++ main.tf
@@ -0,0 +1,15 @@
+locals {
+  name = var.name_postfix == null ? var.name : format("%s-%s", var.name, var.name_postfix)
+}
+
+resource "huaweicloud_obs_bucket" "main" {
+  bucket        = local.name
+  region        = var.region
+  storage_class = var.storage_class
+  acl           = var.acl
+  versioning    = var.versioning
+  force_destroy = var.force_destroy
+
+  tags = var.tags
+}
+
